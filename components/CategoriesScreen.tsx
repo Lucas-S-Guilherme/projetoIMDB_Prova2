706cd809dfa93bc80d0c30bd337cbc3894d6295f
Lucas Guilherme <lucassguilherme159@gmail.com>
2025-02-28 21:08:41 -0400

    funcionando tudo

--- components/CategoriesScreen.tsx
+++ components/CategoriesScreen.tsx
@@ -1,15 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList, StyleSheet, ActivityIndicator, TouchableOpacity } from 'react-native';
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  ActivityIndicator,
+  TouchableOpacity,
+} from 'react-native';
 import axios from 'axios';
 import { StackNavigationProp } from '@react-navigation/stack';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// Defina o tipo para os gêneros
 type Genre = {
   id: number;
   name: string;
 };
 
-// Defina o tipo para a navegação
 type RootStackParamList = {
   Categories: undefined;
   CategoryDetails: { genreId: number; genreName: string };
@@ -19,6 +25,29 @@ type CategoriesScreenNavigationProp = StackNavigationProp<RootStackParamList, 'C
 
 type Props = {
   navigation: CategoriesScreenNavigationProp;
+};
+
+// Mapeamento de ícones para categorias
+const genreIcons: { [key: string]: string } = {
+  Action: 'movie-filter',
+  Adventure: 'terrain',
+  Animation: 'animation',
+  Comedy: 'sentiment-very-satisfied',
+  Crime: 'security',
+  Documentary: 'description',
+  Drama: 'theaters',
+  Family: 'family-restroom',
+  Fantasy: 'auto-awesome',
+  History: 'history',
+  Horror: 'mood-bad',
+  Music: 'music-note',
+  Mystery: 'visibility-off',
+  Romance: 'favorite',
+  'Science Fiction': 'science',
+  'TV Movie': 'live-tv',
+  Thriller: 'whatshot',
+  War: 'public',
+  Western: 'landscape',
 };
 
 const CategoriesScreen: React.FC<Props> = ({ navigation }) => {
@@ -58,6 +87,12 @@ const CategoriesScreen: React.FC<Props> = ({ navigation }) => {
             style={styles.genreItem}
             onPress={() => navigation.navigate('CategoryDetails', { genreId: item.id, genreName: item.name })}
           >
+            <Icon
+              name={genreIcons[item.name] || 'movie'} // Ícone padrão para categorias sem ícone específico
+              size={32}
+              color="#e50914"
+              style={styles.genreIcon}
+            />
             <Text style={styles.genreName}>{item.name}</Text>
           </TouchableOpacity>
         )}
@@ -70,14 +105,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
+    backgroundColor: '#1c1c1c',
   },
   genreItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
     padding: 16,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
+    marginBottom: 8,
+    backgroundColor: '#333',
+    borderRadius: 8,
+  },
+  genreIcon: {
+    marginRight: 16,
   },
   genreName: {
     fontSize: 18,
+    color: '#fff',
   },
 });
 
